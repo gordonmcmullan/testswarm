f535252e040cdfeb93aeaebd5a1481b65fee8cbc
Gordon McMullan <mcmulg20@Fedora14.(none)>
2011-02-14 11:48:38 +0000

    commit Prior to merging

---
+++ scripts/qunit_submit.sh
@@ -0,0 +1,1 @@
+curl -D "state=runjob&browser=popular&max=1&output=dump&user=hudson&auth=903943a4da8a443d7213bd6531718628407e8c9d&suites[]=QUnit&urls[]=http%3A%2F%2Flocalhost%3A8999%2Ftest%2Fqunit%2Ftest%2F" http://localhost:8999/
